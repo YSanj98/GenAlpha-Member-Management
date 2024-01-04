d5365a371890f68445e69c33ed4af75d965e3e8c
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2024-01-04 23:08:27 +0530

    imports

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const path = require('path'); 
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config({ path: path.join(__dirname, 'config', '.env') }); // Define the path to your .env file
   }
-
-  module.exports = app;
+module.exports = app;
