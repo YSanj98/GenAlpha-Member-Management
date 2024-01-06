748691529b9014b13ea06ff37f8d6423fbdf7dcd
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2024-01-06 23:39:29 +0530

    use api in app

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const cors = require("cors");
-const path = require('path'); 
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const user = require("./controller/userController");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/", express.static(path.join(__dirname, "static")));
 
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config({ path: path.join(__dirname, 'config', '.env') }); // Define the path to your .env file
-  }
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config({ path: path.join(__dirname, "config", ".env") }); // Define the path to your .env file
+}
+
+app.use("/api", user);
+
+
+
 module.exports = app;
