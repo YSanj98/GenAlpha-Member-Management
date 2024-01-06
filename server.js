1b99c05d5f1137674e02c977bda22caadb0e32fb
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2024-01-06 23:38:16 +0530

    server and db connection

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
-const app = require('./app');
-const connectDb = require('./database/database');
+const app = require("./app");
+const connectDb = require("./database/database");
 
-if(process.env.NODE_ENV == 'production') {
-    require('dotenv').config({ path: path.join(__dirname, 'config', '.env') }); // Define the path to your .env file
-  }
+if (process.env.NODE_ENV == "production") {
+  require("dotenv").config({ path: path.join(__dirname, "config", ".env") }); 
+}
 
-  connectDb();
+connectDb();
 
-  const server = app.listen(process.env.PORT, () => {
-    console.log(`App running on http://localhost:${process.env.PORT}`);
-    });
+const server = app.listen(process.env.PORT, () => {
+  console.log(`App running on http://localhost:${process.env.PORT}`);
+});
 
-    process.on('uncaughtException', (err) => {
-        console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
-        console.log(err.name, err.message);
-        process.exit(1);
-      });
-    
-    process.on('unhandledRejection', (err) => {
-        console.log('UNHANDLED REJECTION! 💥 Shutting down...');
-        console.log(err.name, err.message);
-        server.close(() => {
-          process.exit(1);
-        });
-      });
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
 
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
