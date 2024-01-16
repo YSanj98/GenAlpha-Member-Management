8e1670d21026c57343ff5bb2be67e3c7eacdbbe3
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2024-01-16 21:20:43 +0530

    forgot password

--- controller/userController.js
+++ controller/userController.js
@@ -3,13 +3,11 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-
 const User = require("../models/User.js");
-
 
 router.post("/register", async (req, res, next) => {
   //data from frontend
-  const { username, email, password, phoneNumber } = req.body; //destructuring
+  const { firstName, lastName, username, email, password, phoneNumber } = req.body; //destructuring
 
   if (!username || typeof username !== "string") {
     return res.json({ status: "error", error: "Invalid username" });
@@ -25,6 +23,8 @@ router.post("/register", async (req, res, next) => {
 
     const response = await User.create({
       username,
+      firstName,
+      lastName,
       email,
       password: hashPassword,
       phoneNumber,
@@ -32,22 +32,25 @@ router.post("/register", async (req, res, next) => {
     console.log("User created successfully: ", response);
   } catch (error) {
     if (error.code === 11000) {
-      return res.status(409).json({ status: "error", error: "Username already in use" });
+      return res
+        .status(409)
+        .json({ status: "error", error: "Username already in use" });
     }
     throw error;
   }
   res.json({ status: "ok" });
 });
 
-router.post("/sign", async (req, res) => {
-  
+router.post("/login", async (req, res) => {
   const { username, password } = req.body;
   console.log(req.body);
-  const user = await User.findOne({ username }).select('+password');
+  const user = await User.findOne({ username }).select("+password");
   console.log(user);
 
   if (!user) {
-    return res.status(401).json({ status: "error", error: "Invalid username or password" });
+    return res
+      .status(401)
+      .json({ status: "error", error: "Invalid username or password" });
   }
 
   if (await bcrypt.compare(password, user.password)) {
@@ -59,9 +62,31 @@ router.post("/sign", async (req, res) => {
       process.env.JWT_SECRET
     );
     return res.json({ status: "ok", data: token });
-  }else{
+  } else {
     return res.json({ status: "error", error: "Invalid username or password" });
   }
 });
+
+router.post("/forgot-password", async (req, res) => {
+  const { token, newPassword } = req.body;
+  console.log(req.body);
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    const { id } = decodedToken;
+
+    // Hash the new password
+    const hashedPassword = await bcrypt.hash(newPassword, 12);
+
+    // Update the user's password in the database
+    await User.findByIdAndUpdate(id, { $set: { password: hashedPassword } });
+
+    res.json({ status: "ok" });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ status: "error", error: "Invalid or expired token" });
+  }
+});
+
 
 module.exports = router;
