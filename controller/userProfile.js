7d9766aa9ab6b7fa13e5cb394295b157b1cb54fe
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2024-01-20 00:39:43 +0530

    personalDetails api endpoint

--- controller/userProfile.js
+++ controller/userProfile.js
@@ -14,4 +14,35 @@ router.get("/getUser", isAuthenticated, async (req, res) =>{
     }
 });
 
+router.post("/personalDetails", isAuthenticated, async (req, res) =>{
+    const { address, gender, birthday, about, portfolioLink } =
+    req.body;
+
+    const user = await User.findById(req.user.id)
+
+    try { 
+        const response = await User.updateOne(
+            { _id: req.user.id },
+            {
+              $set: {
+                address,
+                gender,
+                birthday,
+                about,
+                portfolioLink,
+              },
+            }
+          );
+        console.log("Details added successfully: ", response);
+        res.status(200).json({ status: "ok" , message: "Details added successfully" });
+      }catch (error) {
+        if (error.code === 11000) {
+          return res
+            .status(409)
+            .json({ status: "error", error: "Username already in use" });
+        }
+        throw error;
+      } 
+});
+
 module.exports = router;
