773e83a04f3e27dc62091cfc9e8a9af1682044cd
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2024-02-10 10:46:43 +0530

    multer configs

---
+++ utils/multer.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const filename = file.originalname.split(".")[0];
+    cb(null, filename + "-" + uniqueSuffix + path.extname(file.originalname));
+    console.log(file);
+  },
+});
+
+exports.upload = multer({ storage: storage });
