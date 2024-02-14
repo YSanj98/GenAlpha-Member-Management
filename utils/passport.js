2ed467a72c1e3d386c62669f10a2e5f56bd6e078
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2024-02-14 23:59:31 +0530

    google authentication util

---
+++ utils/passport.js
@@ -0,0 +1,25 @@
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const passport = require("passport");
+require('dotenv').config()
+
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: process.env.CLIENT_ID ,
+			clientSecret: process.env.CLIENT_SECRET,
+			callbackURL: "/auth/google/callback",
+			scope: ["profile", "email"],
+		},
+		function (accessToken, refreshToken, profile, callback) {
+			callback(null, profile);
+		}
+	)
+);
+
+passport.serializeUser((user, done) => {
+	done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+	done(null, user);
+});
