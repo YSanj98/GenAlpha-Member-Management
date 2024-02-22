46ef99713663990bba700d1027cbb0f468f89ad1
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2024-02-22 13:18:17 +0530

    CORS origin

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(
   cors(
     {
-      origin: "http://localhost:3000",
+      origin: " https://generationalpha.info/",
       methods: "GET,POST,PUT,DELETE",
       credentials: true,
     }
