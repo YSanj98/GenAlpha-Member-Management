7bf5e4276fc3209b809f2c6cad8dd9d89a66252b
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2024-04-10 21:20:29 +0530

    image upload location changed

--- utils/multer.js
+++ utils/multer.js
@@ -3,7 +3,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/");
+    cb(null, "public/images/");
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
