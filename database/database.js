56fc5a48d9fb5f5caee237ad7221a811b77405c4
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2024-04-10 21:19:48 +0530

    used singleton pattern

--- database/database.js
+++ database/database.js
@@ -1,9 +1,21 @@
 const mongoose = require("mongoose");
 
-const connectionDb = () => {
-    mongoose.connect(process.env.DB_URL)
-        .then(() => console.log('MongoDB connected'))
-        .catch(err => console.log(err));
+// Database Connection Singleton Pattern 
+let dbConnection = null;
+
+const connectionDb = async () => {
+    try {
+        if (!dbConnection) {
+            dbConnection = await mongoose.connect(process.env.DB_URL);
+            console.log('MongoDB connected');
+        }
+        return dbConnection;
+
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+        throw error;
+    }
 }
+
 
 module.exports = connectionDb;
