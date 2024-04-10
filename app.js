cd6f879717f852a094d37c89bc145441f91eedbb
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2024-04-10 21:18:09 +0530

    used env variable for cors

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const user = require("./controller/userController");
 const settings = require("./controller/settingController");
 const profile = require("./controller/userProfile");
 const landing = require("./controller/landingPageController");
+const mentor = require("./controller/mentorController");
 
 
 const app = express();
@@ -17,7 +18,7 @@ const app = express();
 app.use(
   cors(
     {
-      origin: "https://generationalpha.info/",
+      origin: process.env.CLIENT_URL,     
       methods: "GET,POST,PUT,DELETE",
       credential: true,
     }
@@ -26,7 +27,8 @@ app.use(
 
 app.use(express.json({limit:'5mb'}));
 app.use(bodyParser.urlencoded({ limit:'5mb',  extended: true }));
-app.use("/", express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public", "images")));
+app.use(express.static('./public/images'));
 app.use(cookieSession({ name: "session", maxAge: 24 * 60 * 60 * 1000, keys:["generationalpha"] }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -39,5 +41,6 @@ app.use("/api", user);
 app.use("/api", settings);
 app.use("/api", profile);
 app.use("/api", landing);
+app.use("/api", mentor);
 
 module.exports = app;
