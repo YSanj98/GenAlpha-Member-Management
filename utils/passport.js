1e89cbf8519a3dde64c5a9cdcdb451b74cbfb00c
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2024-05-29 20:42:04 +0530

    formatted document

--- utils/passport.js
+++ utils/passport.js
@@ -1,20 +1,27 @@
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const GoogleStrategy = require("passport-google-oauth2").Strategy;
 const passport = require("passport");
-require('dotenv').config()
+require("dotenv").config();
+
+
 
 passport.use(
 	new GoogleStrategy(
-		{
-			clientID: process.env.CLIENT_ID ,
-			clientSecret: process.env.CLIENT_SECRET,
-			callbackURL: "/auth/google/callback",
-			scope: ["profile", "email"],
-		},
-		function (accessToken, refreshToken, profile, callback) {
-			callback(null, profile);
-		}
+	  {
+		clientID: process.env.CLIENT_ID,
+		clientSecret: process.env.CLIENT_SECRET,
+		callbackURL: "/auth/google/callback",
+		passReqToCallback: true
+	  },
+	  (request, accessToken, refreshToken, profile, done) => {
+		// Here, 'request' is the optional request object, you can use it if needed
+		console.log(profile);
+		return done(null, profile);
+	  }
 	)
-);
+  );
+  
+
+
 
 passport.serializeUser((user, done) => {
 	done(null, user);
@@ -23,3 +30,5 @@ passport.serializeUser((user, done) => {
 passport.deserializeUser((user, done) => {
 	done(null, user);
 });
+
+module.exports = passport;
