6e6416eb483c851381a01faa335ff0fe1d808f78
Yasindu Sanjeewa <yasindusanjeewa8@gmail.com>
2024-05-29 20:42:28 +0530

    googleSign route

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const bodyParser = require("body-parser");
-const passport = require("passport");
+const passport = require('./utils/passport')
 const cookieSession = require("cookie-session");
 const dotenv = require("dotenv");
+
 
 
 const user = require("./controller/userController");
@@ -12,29 +13,34 @@ const settings = require("./controller/settingController");
 const profile = require("./controller/userProfile");
 const landing = require("./controller/landingPageController");
 const mentor = require("./controller/mentorController");
+const googleSign = require('./controller/googleSign')
 
 
 const app = express();
 
 app.use(
-  cors(
-    {
-      origin: ["https://generationalpha.info"],
-      methods: "GET,POST,PUT,DELETE",
-      credential: true,
-    }
-  )
+  cors({
+    origin: process.env.CLIENT_URL_TEST,
+    methods: "GET,POST,PUT,DELETE",
+    credential: true,
+  })
 );
 
-app.use(express.json({limit:'5mb'}));
-app.use(bodyParser.urlencoded({ limit:'5mb',  extended: true }));
+app.use(express.json({ limit: "5mb" }));
+app.use(bodyParser.urlencoded({ limit: "5mb", extended: true }));
 app.use(express.static("public/images"));
-app.use(cookieSession({ name: "session", maxAge: 24 * 60 * 60 * 1000, keys:["generationalpha"] }));
+app.use(
+  cookieSession({
+    name: "session",
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: ["generationalpha"],
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
 if (process.env.NODE_ENV !== "production") {
-  dotenv.config({ path: path.join(__dirname, "config", ".env") }); // Define the path to your .env file
+  dotenv.config({ path: path.join(__dirname, ".env") }); // Define the path to your .env file
 }
 
 app.use("/api", user);
@@ -42,5 +48,6 @@ app.use("/api", settings);
 app.use("/api", profile);
 app.use("/api", landing);
 app.use("/api", mentor);
+app.use("/auth", googleSign)
 
 module.exports = app;
